34cdad63602421a1ad6ef90f87fbade66c9d562f
Ty Petrochko <ty.petrochko@gmail.com>
2016-01-10 10:09:46 -0500

    Finished adaline stochastic gradient descent example

--- adaline.py
+++ adaline.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import seed
 
 # Just a description of some of the parameters
 #   -- Paramaters --
@@ -23,7 +24,7 @@ import numpy as np
 #
 
 class AdalineGD(object):
-    """A python adaptive linear neuron classifier!
+    """A python adaptive linear neuron classifier, using gradient descent!
         
         eta : float, training rate 0.0-1.0
         n_iter : int, num of training passes
@@ -50,6 +51,78 @@ class AdalineGD(object):
             cost = (errors**2).sum() / 2.0
             self.cost_.append(cost)
         return self
+
+    def net_input(self, X):
+        'Calculate net input'
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        'Return class label after unit step'
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+class AdalineSGD(object):
+    """A python adaptive linear neuron classifier, using stochastic gradient descent!
+        
+        eta : float, training rate 0.0-1.0
+        n_iter : int, num of training passes
+    """
+
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_init = False # are weights initialized yet?
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, X, y):
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost)/len(y)
+            self.cost_.append(avg_cost)
+        return self
+
+    def partial_fit(self, X, y):
+        """ Fit training data without re-init'ing weights """
+
+        if not self.w_init:
+            self._initialize_weights(X.shape[1])
+            if y.ravel().shape[0] > 1:
+                for xi, target in zip(X, y):
+                    self._update_weights(xi, target)
+            else:
+                self._update_weights(X, y)
+            return self
+
+    def _shuffle(self, X, y):
+        """ Shuffle training data """
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        """ Init all weights to zeros """
+        self.w_ = np.zeros(1 + m)
+        self.w_init = True
+
+    def _update_weights(self, xi, target):
+        """ Apply adaline learning rule to update weights """
+        output = self.net_input(xi)
+        error = target - output
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        return cost
+
+    def activation(self, X):
+        """ Compute linear activation"""
+        return self.net_input(X)
 
     def net_input(self, X):
         'Calculate net input'
--- adaline_test.py
+++ adaline_test.py
@@ -40,6 +40,7 @@ plt.legend(loc='upper left')
 plt.show()
 """
 
+"""
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
 ada1 = ad.AdalineGD(n_iter=10, eta=0.01).fit(X, y)
 ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
@@ -47,11 +48,31 @@ ax[0].set_xlabel('Epochs')
 ax[0].set_ylabel('log(Sum-squared-error)')
 ax[0].set_title('Adaline learning rate 0.01')
 
+
 ada2 = ad.AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
 ax[1].plot(range(1, len(ada2.cost_) + 1), np.log10(ada2.cost_), marker='o')
 ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('log(Sum-squared-error)')
 ax[1].set_title('Adaline learning rate 0.0001')
 
+plt.show()
+"""
+
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = ad.AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada.fit(X_std, y)
+visualize.plot_decision_regions(plt, X_std, y, classifier=ada)
+plt.title('Adaline stochastic gradient descent')
+plt.xlabel('sepal length standardized')
+plt.ylabel('petal length standardized')
+plt.legend(loc='upper left')
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-errors')
 plt.show()
 
